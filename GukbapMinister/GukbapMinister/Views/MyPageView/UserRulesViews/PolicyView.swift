0ff5fb1e8029cb1929f9ce5059efd7c1f3e1dfba
MartinLeeSJ <tjr2922@gmail.com>
2023-03-08 18:15:27 +0900

    [Feat] Update Policy View

--- GukbapMinister/GukbapMinister/Views/MyPageView/UserRulesViews/PolicyView.swift
+++ GukbapMinister/GukbapMinister/Views/MyPageView/UserRulesViews/PolicyView.swift
@@ -13,75 +13,80 @@ struct PolicyView: View {
     var body: some View {
         NavigationStack {
             VStack(alignment: .center) {
-
-                    List {
-                        
-                        NavigationLink {
-                            ServiceTermsView()
-                        } label: {
-                            Text("서비스 이용약관")
-                        }
-                        .listRowSeparator(.hidden)
-
-                        
-                        
-                        NavigationLink {
-                            PrivacyPolicyView()
-                        } label: {
-                            Text("개인정보 처리 방침")
-                        }
-                        .listRowSeparator(.hidden)
-
-                        
-                        
-                        NavigationLink {
-                            LocationBasedServicePolicyView()
-                        } label: {
-                            Text("위치기반 서비스 이용약관")
-                        }
-                        .listRowSeparator(.hidden)
-
-                        
-                        
-                        NavigationLink {
-                            CommunityGuideLineView()
-                        } label: {
-                            Text("커뮤니티 가이드라인")
-                        }
-                        .listRowSeparator(.hidden)
-
-                        
-                        
-                        NavigationLink {
-                            OpenSourceView()
-                        } label: {
-                            Text("오픈소스 라이선스")
-                        }
-                        .listRowSeparator(.hidden)
-                        
-                        NavigationLink {
-                            AppMakersInfoView()
-                        } label: {
-                            Text("국밥부장관을 만든사람들")
-                        }
-                        .listRowSeparator(.hidden)
-
-                        
-                        Text("버전 정보 1.0.0")
-                            .fontWeight(.semibold)
-                            .listRowSeparator(.hidden)
-
-                        
+                
+                List {
+                    NavigationLink {
+                        ServiceTermsView()
+                    } label: {
+                        Text("서비스 이용약관")
                     }
-                    .listStyle(.plain)
+                    .listRowSeparator(.hidden)
                     
-
+                    
+                    
+                    NavigationLink {
+                        PrivacyPolicyView()
+                    } label: {
+                        Text("개인정보 처리 방침")
+                    }
+                    .listRowSeparator(.hidden)
+                    
+                    
+                    
+                    NavigationLink {
+                        LocationBasedServicePolicyView()
+                    } label: {
+                        Text("위치기반 서비스 이용약관")
+                    }
+                    .listRowSeparator(.hidden)
+                    
+                    
+                    
+                    NavigationLink {
+                        CommunityGuideLineView()
+                    } label: {
+                        Text("커뮤니티 가이드라인")
+                    }
+                    .listRowSeparator(.hidden)
+                    
+                    
+                    
+                    NavigationLink {
+                        OpenSourceView()
+                    } label: {
+                        Text("오픈소스 라이선스")
+                    }
+                    .listRowSeparator(.hidden)
+                    
+                    NavigationLink {
+                        AppMakersInfoView()
+                    } label: {
+                        Text("국밥부장관을 만든사람들")
+                    }
+                    .listRowSeparator(.hidden)
+                    
+                    Link(destination: URL(string:"https://instagram.com/gukbapminister_official?igshid=YmMyMTA2M2Y=")!) {
+                        Text("국밥부장관 인스타그램")
+                    }
+                    .listRowSeparator(.hidden)
+                    
                 }
+                .listStyle(.plain)
+                
+                Spacer()
+                
+                Divider()
+                Text("버전정보: V 1.0.0")
+                    .font(.title3)
+                    .padding(.bottom, 30)
+                
+                
+            }
             .navigationBarTitleDisplayMode(.inline)
             .navigationTitle("앱 정보")
         }
-    
-
+        
+        
         
     }
 }
